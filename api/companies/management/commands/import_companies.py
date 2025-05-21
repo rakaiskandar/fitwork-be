c5304eaff12fff0ae8fdbb90865a5d85ea697e00
rakaiskandar <iskandarraka4@gmail.com>
2025-05-21 17:09:44 +0700

    feat: add company logos

--- api/companies/management/commands/import_companies.py
+++ api/companies/management/commands/import_companies.py
@@ -1,4 +1,6 @@
 import json
+import requests
+from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
 from api.companies.models import Company
 
@@ -24,6 +26,14 @@ class Command(BaseCommand):
                     "culture_keywords": entry.get("culture_keywords", [])
                 }
             )
+            
+            logo_url = entry.get("logo")
+            if logo_url and (created or not company.logo):
+                resp = requests.get(logo_url)
+                if resp.status_code == 200:
+                    filename = logo_url.split("/")[-1]
+                    company.logo.save(filename, ContentFile(resp.content), save=True)
+
             if created:
                 count += 1
 
